94683026796073f731e2751ca1c42c50bd21d9df
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2024-03-15 09:11:23 +0900

    Initial Commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const form = document.getElementById("form");
+
+const approvedImg = '<i class="fa-solid fa-check"></i>';
+const reprovedImg = '<i class="fa-solid fa-xmark"></i>';
+
+const inputActivity = document.getElementById("activity-input");
+const inputGrade = document.getElementById("grade-input");
+
+const minGrade = prompt("Insert minimum grade: ");
+let lines = "";
+const gradeArray = [];
+const activityArray = [];
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    insertTable();
+    tableUpdate();
+    calcFinalGrade();
+    gradeUpdate()
+
+    inputActivity.value = "";
+    inputGrade.value = "";
+})
+
+function insertTable() {
+    if(activityArray.includes(inputActivity.value)){
+        alert(`${inputActivity.value} has already been entered.`);
+    }
+    else {
+        gradeArray.push(parseFloat(inputGrade.value));
+        activityArray.push(inputActivity.value);
+
+        let line = "<tr>";
+        line += `<td> ${inputActivity.value} </td>`;
+        line += `<td> ${inputGrade.value} </td>`;
+        line += `<td> ${inputGrade.value >= minGrade ? approvedImg : reprovedImg} </td>`;
+        line += "</tr>";
+    
+        lines += line;
+    }
+}
+
+function tableUpdate(){
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = lines;
+}
+
+function gradeUpdate() {
+    const finalGrade = calcFinalGrade();
+
+    const finalGradeLine = document.getElementById("final-grade").innerHTML = finalGrade;
+    const finalGradeImg = document.getElementById("final-grade-img").innerHTML = finalGrade >= minGrade ?  approvedImg : reprovedImg;
+}
+
+function calcFinalGrade(){
+    let total = 0;
+
+    for(let i=0; i < gradeArray.length; i++){
+        total += gradeArray[i];
+    }
+
+    return total / gradeArray.length; 
+}
